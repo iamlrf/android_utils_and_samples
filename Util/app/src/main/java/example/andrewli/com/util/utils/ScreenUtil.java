affb245c7e57ba3ed71d9bb73dd336dc0687eab5
andrewli <andrewli1@NKGWANDREWLI01.citrite.net>
2016-06-27 13:06:56 +0800

    add screenUtil

---
+++ Util/app/src/main/java/example/andrewli/com/util/utils/ScreenUtil.java
@@ -0,0 +1,24 @@
+package example.andrewli.com.puzzle.util;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+
+public class ScreenUtil {
+
+    public static DisplayMetrics getScreenSize(Context context) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager wm = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        display.getMetrics(metrics);
+        return metrics;
+    }
+
+    public static float getDeviceDensity(Context context) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager wm = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(metrics);
+        return metrics.density;
+    }
+}
