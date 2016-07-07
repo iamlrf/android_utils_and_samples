04e2b08fa739878d94d045588d73bbecc5d5089b
iamlrf <iamlrf@163.com>
2016-07-07 09:38:44 +0800

    add app template

---
+++ UncaughtEx/app/src/main/java/uncaughtex/andrewli/com/uncaughtex/MainActivity.java
@@ -0,0 +1,14 @@
+package uncaughtex.andrewli.com.uncaughtex;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+    }
+
+}
